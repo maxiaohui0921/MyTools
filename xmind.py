aa511395561b178f1900cd598733661368d3cf16
maxiaohui0921 <38674823+maxiaohui0921@users.noreply.github.com>
2018-04-25 21:02:43 +0800

    访问xmind的各个节点

---
+++ xmind.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from mekk.xmind import XMindDocument
+import time
+
+xfile=r'C:\Users\maxh\Documents\GitHub\yutu\webchat.xmind'
+
+def getSheet(file):
+    xmindf=XMindDocument.open(file)
+    sheet=xmindf.get_first_sheet()
+    print "画布名称：",sheet.get_title()
+    return sheet
+
+def getRoot(sheet):
+    root=sheet.get_root_topic()
+    return root
+
+def getSubs(parent):
+    children=[]
+    for i in parent.get_subtopics():
+        children.append(i)
+    if len(children)>0:
+        return children
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    sheet=getSheet(xfile)
+    print getRoot(sheet).get_title()
+    for i in getSubs(getRoot(sheet)):
+        print i.get_title()
+        if len(getSubs(i))>0:
+            for j in getSubs(i):
+                print j.get_title()
